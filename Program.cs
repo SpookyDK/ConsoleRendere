58adbfc01462d4730aab22b3f154b2e22319c8f1
SpookyDK <esben9541@gmail.com>
2023-01-23 19:05:14 +0100

    Triangle

--- Program.cs
+++ Program.cs
@@ -14,12 +14,14 @@ namespace ConsoleRendere
         Console.CursorVisible = false;
             Console.Clear();
             Point punkt1 = new Point(100, 9);
-            Point punkt2 = new Point(1, 1);
+            Point punkt2 = new Point(1, 7);
+            Point punkt3 = new Point(20, 30);
             int width = Console.WindowWidth;
             int height = Console.WindowHeight;
             Console.WriteLine(height + " " + width );
             Console.WriteLine(punkt1.x + " " + punkt1.y);
-            DrawLine(punkt1, punkt2);
+            //DrawLine(punkt1, punkt2);
+            DrawTriangle(punkt1, punkt2, punkt3);
             
 
 
@@ -28,12 +30,13 @@ namespace ConsoleRendere
       static void DrawLine(Point point1, Point point2)
       {
             float Angle = (float)(point2.y - point1.y) / (float)(point2.x - point1.x);
+            int offset = (int)(point2.y - Angle * point2.x);
             Console.WriteLine(Angle);
             if (point2.x < point1.x)
             {
                 for (int i = point2.x; i < point1.x; i++)
                 {
-                    Console.SetCursorPosition(i, (int)(i * Angle));
+                    Console.SetCursorPosition(i, (int)(i * Angle) + offset);
                     Console.Write("*");
                 }
             }
@@ -47,6 +50,12 @@ namespace ConsoleRendere
 
             }
             
+      }
+      static void DrawTriangle(Point point1, Point point2, Point point3)
+      {
+        DrawLine(point1, point2);
+        DrawLine(point1, point3);
+        DrawLine(point3, point2);
       }
 
    }
